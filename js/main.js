fc01fc7f564ace763f69cfa4c57201647096a74b
Derp88 <62079853+Derp88@users.noreply.github.com>
2024-01-04 06:19:02 -0500

    Hide based off width

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
-function initPage() {
-    //Mobile Layout
-    //Get all the divs we want to hide
-    let elementsToHide = document.getElementsByClassName("navChild");
-    //For every div
-    for (let i = 0; i < elementsToHide.length; i++){
-        elementsToHide[i].style.display = "none";
-    }
-    
-    //elementsToHide.childNodes.style.display = "none";
+let navBarElements = document.getElementsByClassName("navChild");
+let windowWidth = window.innerWidth;
+const maxMobileWidth = 800;
 
-    //Desktop Layout
+
+function setPage() {
+    //Mobile Layout
+    if(windowWidth < maxMobileWidth){
+        hideNavBar();
+    }else{ //Desktop Layout
+        showNavBar();
+    }
+}
+
+function showNavBar(){
+    //For every node in the nav bar
+    for (let i = 0; i < navBarElements.length; i++){
+        navBarElements[i].style.display = "flex";
+    }
+}
+function hideNavBar(){
+    //For every node in the nav bar
+    for (let i = 0; i < navBarElements.length; i++){
+        navBarElements[i].style.display = "none";
+    }
 }
 
 
 
-
-
-
-initPage();
+setPage();
