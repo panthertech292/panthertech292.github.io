c597b89f16f09566c9a3a6023102b451aaceeaa4
Derp88 <62079853+Derp88@users.noreply.github.com>
2024-01-04 06:04:06 -0500

    Hide fix 2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 function initPage() {
     //Mobile Layout
-    let elementsToHide = document.getElementsByClassName("navContainer").childNodes;
+    let elementsToHide = document.getElementsByClassName("navChild");
     elementsToHide.style.display = "none";
-    elementsToHide.childNodes.style.display = "none";
+    //elementsToHide.childNodes.style.display = "none";
 
     //Desktop Layout
 }
+
+
+
+
+
+
+initPage();
