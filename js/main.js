b1409c660bce87b430d23a1b56b919a0406a59b8
Derp88 <62079853+Derp88@users.noreply.github.com>
2024-01-04 06:09:14 -0500

    Hide fix 3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 function initPage() {
     //Mobile Layout
+    //Get all the divs we want to hide
     let elementsToHide = document.getElementsByClassName("navChild");
-    elementsToHide.style.display = "none";
+    //For every div
+    for (let i = 0; i < elementsToHide.length; i++){
+        elementsToHide[i].style.display = "none";
+    }
+    
     //elementsToHide.childNodes.style.display = "none";
 
     //Desktop Layout
